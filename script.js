677bbdc10184fb56e293a1360278fb05cd0ef17c
Suhani Srivastava <suhanisrivastava9792@gmail.com>
2023-02-11 20:39:32 +0530

    commit

--- script.js
+++ script.js
@@ -7,21 +7,43 @@ let arr = [
 ];
 
 function PrintStudentswithMap() {
-  //Write your code here , just console.log
+  arr.map(obj => {
+    if(obj.marks > 50){
+      console.log(obj);
+    }
+  });
 }
 
 function PrintStudentsbyForEach() {
-  //Write your code here , just console.log
+  arr.forEach(element => {
+    if(element.marks > 50){
+      console.log(element);
+    }
+  });
 }
 
 function addData() {
-  //Write your code here, just console.log
+  let obj = {id:4,name:"suhani",age:"20",marks:45};
+  arr.push(obj);
+  console.log(arr);
 }
 
 function removeFailedStudent() {
-  //Write your code here, just console.log
+  for(let i = 0; i <= arr.length; i++){
+    if(arr[i].marks <= 50){
+      arr.splice(i, i + 1);
+    }
+  }
+  console.log(arr);
 }
 
 function concatenateArray() {
-  //Write your code here, just console.log
+  let ids = arr.length;
+  let arr1 = [
+    { id: ids + 1, name: "suhani", age: "22", marks: 85 },
+    { id: ids + 2, name: "nishank", age: "28", marks: 80 },
+    { id: ids + 3, name: "nishu", age: "24", marks: 45 },
+  ];
+  let array = arr.concat(arr1);
+  console.log(array)
 }
